84a8972436f4e57bd21efd7f680e47aa642ab551
Henrik Gregersen <hgr@microsolutions.dk>
2024-09-27 15:17:43 +0200

    Restructure for 365FO

--- dag/dag.py
+++ dag/dag.py
@@ -1,40 +1,6 @@
 from typing import List, Set   #, Type
 from collections import defaultdict
 
-# Basisklasse der indeholder varenummer. 
-class ProductDagNode:
-    def __init__(self):
-        self.__nodeId = ''
-        self.plc: int = None
-
-    def getNodeId(self) -> str:
-        return self.__nodeId
-    
-    def setNodeId(self, nodeId : str):
-        self.__nodeId = nodeId
-
-    @classmethod
-    def construct(cls, vareNr : str, plc : int):
-        newNode = cls()
-        newNode.setNodeId(vareNr)
-        newNode.plc = plc
-        return newNode
-
-#TODO: lav en subclass af ProductDagNode hvor du har PLC properties på og lav så en måde at få sammenlignet værdierne i traverseringen
-
-# klasse der indeholder link mellem parrent og child noder    
-class ProductDagNodesEdge:    
-    def __init__(self, fromNode : ProductDagNode, toNode: ProductDagNode):
-        # Definerer fromNode og toNode som private
-        self.__fromNode = fromNode
-        self.__toNode = toNode
-
-    def getFromNode(self) -> ProductDagNode:
-        return self.__fromNode
-    
-    def getToNode(self) -> ProductDagNode:
-        return self.__toNode
-
 class ProductDAGTraverseController:
     def __init__(self) -> None:
         self.__todoNodesSet: Set[str] = None # str skal være nodeid aka varenr i 365
